33df17286339c8fa4585d6cf3ea832e06bc5d59e
Eliz Marie Manalo <tech.ezly@gmail.com>
2022-06-25 02:06:45 +0800

    make header sticky

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,18 @@
-// Header menu
-
+// Header Menu
 $(document).ready(function() {
 
     $(".header__hamburger").on('click', function() {
         $('.header__wrapper').toggleClass('openmenu');
     })
 }) 
+
+// Sticky Header
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+        $("header").addClass("stickyHeader");
+        }
+        else{
+        $("header").removeClass("stickyHeader");
+        }
+})
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-$(document).ready((function(){$(".header__hamburger").on("click",(function(){$(".header__wrapper").toggleClass("openmenu")}))}));
+$(document).ready((function(){$(".header__hamburger").on("click",(function(){$(".header__wrapper").toggleClass("openmenu")}))})),$(window).scroll((function(){$(this).scrollTop()>1?$("header").addClass("stickyHeader"):$("header").removeClass("stickyHeader")}));
 //# sourceMappingURL=script.js.map
